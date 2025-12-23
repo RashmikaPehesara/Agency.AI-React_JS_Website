3e85d3765b8fc4e4121fc6261ad6fc19daf6552a
RashmikaPehesara <rashmikapehesara2022@gmail.com>
2025-12-23 18:14:00 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,81 @@
+import React, {useRef, useState, useEffect} from 'react'
+import Navbar from './Components/Navbar'
+import Hero from './Components/Hero'
+import TrustedBy from './Components/TrustedBy'
+import Services from './Components/Services'
+import OurWork from './Components/OurWork'
+import Teams from './Components/Teams'
+import Contacts from './Components/Contacts'
+import {Toaster} from 'react-hot-toast'
+import Footer from './Components/Footer'
+
+function App() {
+
+   const [theme, setTheme] = useState(localStorage.getItem('theme')? localStorage.getItem('theme'): 'light')
+
+  const dotRef= useRef(null)
+  const outlineRef = useRef(null)
+
+  // Refs for custom cursor Position tracking
+const mouse = useRef({x: 0, y: 0})
+const position = useRef({x: 0, y: 0})
+
+
+useEffect(()=>{
+const handleMouseMove = (e)=>{
+mouse.current.x = e.clientX
+mouse.current.y = e.clientY
+}
+
+document.addEventListener('mousemove', handleMouseMove)
+
+const animate = () => {
+position.current.x += (mouse.current.x - position.current.x) * 0.3
+position.current.y += (mouse.current.y - position.current.y) * 0.2
+
+if(dotRef.current && outlineRef.current){
+dotRef.current.style.transform = `translate3d(${mouse.current.x - 6}px, ${mouse.current.y - 6}px, 0)` 
+outlineRef.current.style.transform = `translate3d(${position.current.x - 20}px, ${position.current.y - 20}px, 0)` 
+
+}
+
+requestAnimationFrame(animate)
+
+}
+
+animate()
+
+return ()=>{
+  document.removeEventListener('mousemove', handleMouseMove)
+}
+}, [])
+   
+
+ 
+  return (
+    <div className='dark:bg-black relative'>
+      <Toaster/>
+      <Navbar theme={theme} setTheme={setTheme}/>
+      <Hero/>
+      <TrustedBy/>
+      <Services/>
+      <OurWork/>
+      <Teams/>
+      <Contacts/>
+      <Footer theme={theme}/>
+
+      {/* Custom Cursor Ring */}
+<div ref={outlineRef} className='fixed top-0 left-0 h-10 w-10
+rounded-full border border-primary pointer-events-none z-[9999]
+'></div>
+
+{/*Custom Cursor Dot*/}
+<div ref={dotRef} className='fixed top-0 left-0 h-3 w-3
+rounded-full bg-primary pointer-events-none z-[9999]'></div>
+
+
+    </div>
+  )
+}
+
+export default App
